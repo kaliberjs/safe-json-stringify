b4116441edb04a3634b0f02d923ec0bc969709c4
Peeke Kuepers <pkuepers@kaliber.net>
2022-07-25 10:10:11 +0200

    Adhere to linting rules

--- example/src/App.universal.js
+++ example/src/App.universal.js
@@ -1,7 +1,7 @@
-import { safeJsonStringify }  from '@kaliber/safe-json-stringify'
+import { safeJsonStringify } from '@kaliber/safe-json-stringify'
 
-const content =  {"title":"Woooppppss","content":"</script><script>alert('gotcha!')</script>"}
+const content = { 'title': 'Woooppppss', 'content': '</script><script>alert("gotcha!")</script>' }
 
 export default function App() {
-  return <div dangerouslySetInnerHTML={{__html: safeJsonStringify(content)}}/>
+  return <div dangerouslySetInnerHTML={{ __html: safeJsonStringify(content) }} />
 }
