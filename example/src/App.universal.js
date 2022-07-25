c455566e577adf9ca7b561c98e8cf885900eca8a
Ramon Mostert <rmostert@kaliber.net>
2022-07-25 09:48:41 +0200

    cleanup console.log

--- example/src/App.universal.js
+++ example/src/App.universal.js
@@ -1,7 +1,7 @@
 import { safeJsonStringify }  from '@kaliber/safe-json-stringify'
 
 const content =  {"title":"Woooppppss","content":"</script><script>alert('gotcha!')</script>"}
-console.log({safeJsonStringify})
+
 export default function App() {
   return <div dangerouslySetInnerHTML={{__html: safeJsonStringify(content)}}/>
 }
